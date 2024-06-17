fb9e89451ee51dedd62e1148be7720cda9c106e4
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-17 01:05:40 -0700

    Add project files.

---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                mathOptionalParameter math = new mathOptionalParameter();
+
+                // user input two numbers
+                Console.Write("Enter the first number: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter the second number (optional): ");
+                string input = Console.ReadLine();
+
+                int num2;
+                // Check if the input is empty
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    // If empty, uset value 0
+                    num2 = 0;
+                }
+                else
+                {
+                    // If not empty, parse the input to integer
+                    num2 = int.Parse(input);
+                }
+
+                // Call the method with the user input
+                int result = math.MathOp(num1, num2);
+
+                Console.WriteLine("Result of the operation: " + result);
+                Console.ReadLine();
+            }
+        }
+
+    }
---
+++ MethodAssignment/mathOptionalParameter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class mathOptionalParameter
+            {
+            // Method to perform a math operation on two integers
+            // 'b' is the optional 
+            public int MathOp(int a, int b = 0)
+            {
+                // addition of the integers
+                int result = a + b;
+                return result;
+            }
+        }
+
+    
+}
